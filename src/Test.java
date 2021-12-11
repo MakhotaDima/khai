5c8da5c0d8aa009bb09ccd1a1efb5e6455a45bd1
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-11 20:42:15 +0300

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,10 @@ public class Test {
 =======
         System.out.println("education");
         //new string
+<<<<<<< HEAD
 >>>>>>> bc0c792 (Test.java added to feature_branch with change on string #2)
+=======
+        System.out.println("DICT");
+>>>>>>> 45514c9 (Test.java added to feature_branch with change on string #3)
     }
 }
