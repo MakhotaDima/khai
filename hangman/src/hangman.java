b2d6e8f6bc8bfac32fe3213367a95fac44cb2d4f
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-15 23:16:59 +0300

    hangman 5-st stage

--- hangman/src/hangman.java
+++ hangman/src/hangman.java
@@ -9,19 +9,22 @@ public class hangman {
         String[] word = {"java", "python", "kotlin", "javascript"};
         int check = 0 + (int) (random() * 4);
         String cor_word = word[check];
-        for (int i = 0; i < 2; i++){
-            System.out.print(cor_word.charAt(i));
+        String[] word2;
+        word2 = new String[cor_word.length()];
+        for (int i = 0; i<cor_word.length(); i++){
+            word2[i] = "-";
         }
-        for (int i = 2; i < cor_word.length(); i++){
-            System.out.print("-");
+        String letter;
+        for (int i = 0; i < 8; i++) {
+            letter = in.next();
+            for (int i1 = 0; i1 < cor_word.length(); i1++){
+                if (letter.equals(cor_word.charAt(i1))){
+                    word2[i1] = letter;
+                }
+            }
+
         }
-        System.out.println("\nGuess the word: ");
-        String java_correct = in.nextLine();
-        if (java_correct.equals(cor_word)){
-            System.out.println("You survived");
-        }
-        else {
-            System.out.println("You died");
-        }
+        System.out.println("You Died");
+
     }
 }
