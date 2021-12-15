519bbeae605ce37d1f406161ba09f8dfa97cb3ba
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-15 22:00:13 +0300

    ChatBot 4-st stage

--- ChatBot/src/ChatBot.java
+++ ChatBot/src/ChatBot.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class ChatBot {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+        boolean answer = false;
+        int answ;
         System.out.println("Hello! My name is spider-man.\n" +
                 "I was created in 12.12.2021.");
         System.out.println("Please, remind me your name.");
@@ -20,5 +22,21 @@ public class ChatBot {
         for (int i = 0; i <= number; i++){
             System.out.println(i + " !");
         }
+        do {
+            System.out.println("2 + 2 = ?");
+            System.out.println("1. 1");
+            System.out.println("2. 2");
+            System.out.println("3. 3");
+            System.out.println("4. 4");
+            answ = in.nextInt();
+            if(answ == 4){
+                answer = true;
+            }
+            else {
+                System.out.println("Please, try again");
+            }
+        }while (answer == false);
+        System.out.println("Great, you right!");
+        System.out.println("Goodbye, have a nice day!");
     }
 }
