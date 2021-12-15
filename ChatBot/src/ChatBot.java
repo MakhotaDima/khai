485d0e22487cb59d7aebb34ec1ea76af62d130a6
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-15 21:46:51 +0300

    ChatBot 2-st stage

--- ChatBot/src/ChatBot.java
+++ ChatBot/src/ChatBot.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello! My name is spider-man.\n" +
                 "I was created in 12.12.2021.");
+        System.out.println("Please, remind me your name.");
+        String name = in.next();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
