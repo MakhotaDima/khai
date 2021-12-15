8a976a60b922bddea4875447ef8c7525b0621bb6
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-16 00:35:32 +0300

    CoffeeMachine 4-st stage

--- CoffeeMachine/src/CoffeeMachine.java
+++ CoffeeMachine/src/CoffeeMachine.java
@@ -4,18 +4,58 @@ public class CoffeeMachine {
     public static void main (String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println("enter Water, Milk, Coffee!");
-        int w = in.nextInt(), m = in.nextInt(), c = in.nextInt();
+        int w = 400, m = 100, c = 12, grn = 550, stakan = 9;
         int all;
-        System.out.println("What the cups?");
-        int cups = in.nextInt();
-        if ((m-(cups*50)) < 0 || (w-(cups*200))<0 || (c-(cups*15)) < 0 ){
-            System.out.println("No, i can take Only " + (Math.min((m/50), Math.min((w/200), (c/15)))));
-        }
-        else if ((Math.min(m/50, Math.min(w/200, c/15))-cups) == 0){
-            System.out.println("Yes, i can doing " + cups + " cups");
-        }
-        else {
-            System.out.println("Yes, i can doing " + cups + " cups and " + (Math.min(m/50, Math.min(w/200, c/15))-cups) + " more");
+        for(;;) {
+            System.out.println("The coffee machine has: " + w + "  Water, " + m + " Milk, " + c + " Coffee, " + grn + " grn, " + stakan + " stakan");
+            System.out.println("What are you doing? buy/fill/take ");
+            String whatwedoing = in.next();
+            if (whatwedoing.equals("buy")){
+                System.out.println("What you buy? 1 - espresso, 2 - Latte, 3 - cappucino");
+                all = in.nextInt();
+                if(all == 1){
+                    if ((w - 250) <0 || (m - 0) < 0 || (c - 16) < 0|| (stakan -1) < 0){
+                        System.out.println("No resurce");
+                    }
+                    else {
+                        w -=250; m-=0; c-=16; stakan-=1; grn+=4;
+                    }
+                }
+                else if (all == 2){
+                    if ((w - 350) <0 || (m - 75) < 0 || (c - 20) < 0|| (stakan -1) < 0){
+                        System.out.println("No resurce");
+                    }
+                    else {
+                        w -=350; m-=75; c-=20; stakan-=1; grn+=7;
+                    }
+                }
+
+                else if(all == 3){
+                    if ((w - 200) <0 || (m - 100) < 0 || (c - 12) < 0|| (stakan -1) < 0){
+                        System.out.println("No resurce");
+                    }
+                    else {
+                        w -=200; m-=100; c-=12; stakan-=1; grn+=6;
+                    }
+                }
+                else {
+                    System.out.println("Incorrect");
+                }
+            }
+            else if (whatwedoing.equals("fill")){
+                System.out.println("Water: ");
+                w = w + in.nextInt();
+                System.out.println("Milk: ");
+                m = m + in.nextInt();
+                System.out.println("Coffee: ");
+                c = c + in.nextInt();
+                System.out.println("Stakan: ");
+                stakan =stakan + in.nextInt();
+
+            }
+            else if (whatwedoing.equals("take")){
+                grn = 0;
+            }
         }
     }
 }
