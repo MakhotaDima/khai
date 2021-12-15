69a05a05a9c022b92d5bdc4a1c2e194c4995a9f1
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-15 22:50:04 +0300

    hangman 2-st stage

--- hangman/src/hangman.java
+++ hangman/src/hangman.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class hangman {
     public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
         System.out.println("The game will be available soon.");
+        String java = "java";
+        System.out.println("Guess the word: ");
+        String java_correct = in.nextLine();
+        if (java_correct.equals(java)){
+            System.out.println("You survived");
+        }
+        else {
+            System.out.println("You died");
+        }
     }
 }
