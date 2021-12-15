b9840e41a94a7cfc61b3add6569bc30b73e949e0
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-15 23:38:02 +0300

    hangman 6-st stage correct

--- hangman/src/hangman.java
+++ hangman/src/hangman.java
@@ -14,20 +14,56 @@ public class hangman {
         for (int i = 0; i<cor_word.length(); i++){
             word2[i] = "-";
         }
+        String[] lost_letter = new String[256];
         String letter;
-
-
+        int test = cor_word.length();
+        int chapt = 0;
+        int t = 0;
+        int c = 0;
         for (int i = 0; i < 8; i++) {
+            test = cor_word.length();
+            t = 0;
             letter = in.next();
             for (int i1 = 0; i1 < cor_word.length(); i1++){
                 if (letter.charAt(0) == cor_word.charAt(i1)){
                     word2[i1] = letter;
+                    t++;
                 }
             }
             for (int i1 = 0; i1 < cor_word.length(); i1++){
                 System.out.print(word2[i1]);
             }
+            for (int i1 = 0; i1 < cor_word.length(); i1++){
+                if (!word2[i1].equals("-")){
+                    test--;
+                }
+                if (test == 0){
+                    System.out.println("YOU Survived!");
+                    System.exit(0);
+                }
+            }
             System.out.println();
+            for (int i1 = 0; i1 < chapt; i1++) {
+                if (lost_letter[i1].charAt(0) == letter.charAt(0)){
+                    c++;
+                }
+            }
+            if (t !=0 ){
+                    i--;
+            }
+            else{
+                if(c != 0){
+                    System.out.println("No improvements");
+                }
+                else {
+                    System.out.println("That letter doesnt appear in the word");
+                }
+            }
+
+            lost_letter[chapt] = letter;
+
+            chapt++;
+
 
         }
         System.out.println("You Died");
