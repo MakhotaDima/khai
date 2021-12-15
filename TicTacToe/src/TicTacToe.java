47f830668a457f0e6b49764a227ab8855aab05ea
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-16 01:05:36 +0300

    TicTacToe 2-st stage

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args){
-        System.out.println("X O X\n" +
-                "O X O\n" +
-                "X X O");
+        Scanner in = new Scanner(System.in);
+        String[][] pole = new String[3][3];
+        System.out.println("Enter pole: ");
+        for (int i = 0; i < 3; i++){
+            for (int i1 = 0; i1 < 3; i1++){
+                pole[i][i1] = in.next();
+            }
+        }
+        for (int i = 0; i < 3; i++){
+            System.out.println("\n----------");
+            for (int i1 = 0; i1 < 3; i1++){
+                System.out.print("|");
+                System.out.print(pole[i][i1]);
+                System.out.print("|");
+            }
+        }
+        System.out.print("\n----------");
     }
 }
