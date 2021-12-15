cdf85992a226c04abf3f7c5d8fb7af4a900b497f
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-15 21:17:00 +0300

    ChatBot 1-st stage

---
+++ ChatBot/src/ChatBot.java
@@ -0,0 +1,6 @@
+public class ChatBot {
+    public static void main(String[] args){
+        System.out.println("Hello! My name is spider-man.\n" +
+                "I was created in 12.12.2021.");
+    }
+}
