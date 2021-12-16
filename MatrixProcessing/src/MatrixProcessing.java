71fa80f56696410a47cfbb626b4f97efd00eca6e
Dima Mahota <d.i.makhota@student.khai.edu>
2021-12-17 00:37:53 +0300

    1-st stage

--- MatrixProcessing/src/MatrixProcessing.java
+++ MatrixProcessing/src/MatrixProcessing.java
@@ -14,19 +14,14 @@ public class MatrixProcessing {
             }
         }
         int n1 = in.nextInt();
-        int m1 = in.nextInt();
-        if (n != n1 || m != m1) {
             Matrix2 = new int[n][m];
             for (int i = 0; i < n; i++) {
                 for (int i1 = 0; i1 < m; i1++) {
-                    Matrix2[i][i1] = in.nextInt() + Matrix[i][i1];
-                    System.out.print(Matrix2[i][i1]);
+                    Matrix2[i][i1] = n1 * Matrix[i][i1];
+                    System.out.print(" " + Matrix2[i][i1]);
                 }
                 System.out.println();
             }
-        }
-        else {
-            System.out.println("ERROR");
-        }
+
     }
 }
